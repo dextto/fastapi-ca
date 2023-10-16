5655886520c738f095547b9e5f444d489ebab134
dexter <dexter.haan@gmail.com>
2023-10-17 08:35:49 +0900

    03-02

---
+++ user/application/user_service.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from http.client import HTTPException
+from ulid import ULID
+
+from user.domain.user import User
+from user.domain.repository.user_repo import IUserRepository
+from user.infra.repository.user_repo import UserRepository
+from utils.crypto import Crypto
+
+
+class UserService:
+    def __init__(self):
+        self.user_repo: IUserRepository = UserRepository()
+        self.ulid = ULID()
+        self.crypto = Crypto()
+
+    def create_user(self, name: str, email: str, password: str):
+        _user = None
+        try:
+            _user = self.user_repo.find_by_email(email)
+        except HTTPException as e:
+            if e.status_code != 422:
+                raise e
+
+        if _user:
+            raise HTTPException(status_code=422)
+
+        now = datetime.now()
+        user: User = User(
+            id=self.ulid.generate(),
+            name=name,
+            email=email,
+            password=self.crypto.encrypt(password),
+            created_at=now,
+            updated_at=now,
+        )
+        self.user_repo.save(user)
+
+        return user
---
+++ user/infra/repository/user_repo.py
@@ -0,0 +1,5 @@
+from user.domain.repository.user_repo import IUserRepository
+
+
+class UserRepository(IUserRepository):
+    pass
