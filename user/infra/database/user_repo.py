6f0bfe18b52383a12c01a0e4886a316fdb0fd7d8
dexter <dexter.haan@gmail.com>
2023-11-05 22:56:40 +0900

    feat: user repository

--- user/infra/database/user_repo.py
+++ user/infra/database/user_repo.py
@@ -1,5 +1,10 @@
 from user.domain.repository.user_repo import IUserRepository
+from user.domain.user import User
 
 
 class UserRepository(IUserRepository):
-    pass
+    def save(self, user: User):
+        pass
+
+    def find_by_email(self, email: str) -> User:
+        pass
