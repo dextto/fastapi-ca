2a1ad23fa05aad1972061ecc3278902fb220d295
dexter <dexter.haan@gmail.com>
2023-11-12 22:06:04 +0900

    refactor: inspect를 이용하여 코드 간소화

--- user/infra/repository/user_repo.py
+++ user/infra/repository/user_repo.py
@@ -3,6 +3,7 @@ from database import SessionLocal
 from user.domain.repository.user_repo import IUserRepository
 from user.domain.user import User as UserVO
 from user.infra.db_models.user import User
+from utils.db_utils import row_to_dict
 
 
 class UserRepository(IUserRepository):
@@ -16,23 +17,15 @@ class UserRepository(IUserRepository):
             updated_at=user.updated_at,
         )
 
-        with SessionLocal() as session:
-            session = SessionLocal()
-            session.add(new_user)
-            session.commit()
+        with SessionLocal() as db:
+            db.add(new_user)
+            db.commit()
 
     def find_by_email(self, email: str) -> UserVO:
-        with SessionLocal() as session:
-            user = session.query(User).filter(User.email == email).first()
+        with SessionLocal() as db:
+            user = db.query(User).filter(User.email == email).first()
 
         if not user:
             raise HTTPException(status_code=422)
 
-        return UserVO(
-            id=user.id,
-            name=user.name,
-            email=user.email,
-            password=user.password,
-            created_at=user.created_at,
-            updated_at=user.updated_at,
-        )
+        return UserVO(**row_to_dict(user))
