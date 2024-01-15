ccb86556cb8a4f2e299126b49a97c5c905ab4188
dextto <dexter.haan@gmail.com>
2024-01-16 00:31:56 +0900

    05-04

--- user/domain/repository/user_repo.py
+++ user/domain/repository/user_repo.py
@@ -27,3 +27,7 @@ class IUserRepository(metaclass=ABCMeta):
     @abstractmethod
     def get_users(self, page: int, items_per_page: int) -> tuple[int, list[User]]:
         raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, id: str):
+        raise NotImplementedError
