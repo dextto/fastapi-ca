e1f2c409511f354561ddb46862d9f9a65111d59d
dextto <dexter.haan@gmail.com>
2024-01-15 23:51:55 +0900

    04-01

--- user/interface/controllers/user_controller.py
+++ user/interface/controllers/user_controller.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
 from user.application.user_service import UserService
@@ -13,12 +14,14 @@ class CreateUserBody(BaseModel):
 
 
 @router.post("", status_code=201)
-def create_user(user: CreateUserBody):
-    user_service = UserService()
+def create_user(
+    user: CreateUserBody,
+    user_service: Annotated[UserService, Depends(UserService)],
+):
     created_user = user_service.create_user(
         name=user.name,
         email=user.email,
-        password=user.password
+        password=user.password,
     )
 
     return created_user
