5244e086c1ee44bce77c4e08418761abf594783c
dextto <dexter.haan@gmail.com>
2024-01-15 23:46:07 +0900

    08-02

---
+++ config.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+    database_username: str
+    database_password: str
+    jwt_secret: str
+
+
+@lru_cache
+def get_settings():
+    return Settings()
